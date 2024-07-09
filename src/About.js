5cf3b01fcbc289eaee0c701087b652d80680ace2
Pranav Mahesh <102569626+pmahesh29@users.noreply.github.com>
2024-07-09 10:51:18 -0500

    Update About.js

--- src/About.js
+++ src/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import myPhoto from './Assets/pranav_photo.jpg';
+import './About.css';
 
 const About = () => (
   <section className="about">
@@ -7,10 +8,8 @@ const About = () => (
       <h2>About</h2>
       <div className="about-content">
         <img src={myPhoto} alt="Pranav Mahesh" className="about-photo" />
-        <p>Hello, I'm Pranav Mahesh, a fourth-year Computer Science major, with a focus on cybersecurity at the University of Arkansas. 
-           I am proficient in a variety of programming languages, including Python, C++, HTML, Java, JavaScript, and TypeScript. 
-           Additionally, as an officer of CyberHogs, I actively participate in Capture The Flag competitions, such as hosting the Razorhack competition at the University of Arkansas.
-           I am eager to delve deeper into the captivating field of cybersecurity, so let's connect and share insights.</p>
+        <p>Hello, I'm Pranav Mahesh, a fourth-year Computer Science major, with a focus on cybersecurity at the University of Arkansas.  
+          <br></br> Welcome to my Portfolio.</p>
       </div>
     </div>
   </section>
