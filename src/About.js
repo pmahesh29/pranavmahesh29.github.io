72c956d9af922466ad4ed0637d4b608b104c1edb
Pranav Mahesh <102569626+pmahesh29@users.noreply.github.com>
2024-07-20 12:30:10 -0500

    Update About.js

--- src/About.js
+++ src/About.js
@@ -8,8 +8,9 @@ const About = () => (
       <h2>About</h2>
       <div className="about-content">
         <img src={myPhoto} alt="Pranav Mahesh" className="about-photo" />
-        <p>Hello, I'm Pranav Mahesh, a fourth-year Computer Science major, with a focus on cybersecurity at the University of Arkansas.  
-          <br></br> Welcome to my Portfolio.</p>
+          <p>Hello, I'm Pranav Mahesh, a fourth-year Computer Science major at the University of Arkansas with a focus on Cybersecurity. I have a passion for technology and security, and I enjoy working on projects that challenge me to learn and grow.</p> 
+          <p>My experience includes internships in data security and software development, where I've developed practical skills in various programming languages and tools. I'm also actively involved in fostering a community of like-minded individuals as the Vice-President of CyberHogs. </p>
+          <p>When I'm not working, you can find me exploring outdoors, going to new coffee shops, or watching movies. I'm always looking for new opportunities to learn and grow, so feel free to reach out to me!</p>
       </div>
     </div>
   </section>
